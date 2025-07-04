076257c13844e424918c2407bc6cb5709bd82413
projectcarabaoph <projectcarabao@gmail.com>
2025-07-04 13:58:20 +0800

    refactor: marketing layout footer

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -34,10 +34,20 @@ const MarketingLayout = ({ children }: { children: ReactNode }) => {
                 {children}
             </main>
             <footer className="border border-white p-2 w-full  max-w-7xl">
-                <nav>
-                    <ul>
-                        <li>
-                            footer
+                <nav className="w-full h-full">
+                    <ul className="list-none p-0 flex justify-between items-center">
+                        <li className="flex flex-row gap-2 justify-between items-center">
+                            <Link href="/">
+                                <MainLogo className="w-8 h-8 fill-neutral-400 hover:fill-neutral-200" />
+                            </Link>
+                        </li>
+                        <li className="flex flex-row gap-2 justify-between items-center">
+                            <Button className="bg-neutral-900 text-neutral-400 hover:text-neutral-200" >
+                                Resume
+                            </Button>
+                            <Button className="bg-neutral-900 text-neutral-400 hover:text-neutral-200" >
+                                <Sun />
+                            </Button>
                         </li>
                     </ul>
                 </nav>
