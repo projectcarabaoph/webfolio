9bcb54bb3e68a44e32d5c0fb688bb5eadeae7e34
projectcarabaoph <projectcarabao@gmail.com>
2025-07-04 22:59:32 +0800

    refactor: utils, app config make process.env inline

--- utils/configs/app.config.ts
+++ utils/configs/app.config.ts
@@ -1,17 +1,9 @@
 import { z } from "zod";
 
 
-const {
-    NEXT_PUBLIC_USER_NAME,
-    NEXT_PUBLIC_USER_DESCRIPTION,
-    NEXT_PUBLIC_USER_DESIGNATION,
-    NEXT_PUBLIC_USER_LOCATION,
-    NEXT_PUBLIC_GITHUB_URL,
-    NEXT_PUBLIC_DAILY_URL,
-    NEXT_PUBLIC_EMAIL_TO,
-} = process.env
 
 export function appConfig() {
+
     return z
         .object({
             name: z.string().min(1),
@@ -23,12 +15,12 @@ export function appConfig() {
             email: z.string().min(1),
         })
         .parse({
-            name: NEXT_PUBLIC_USER_NAME,
-            description: NEXT_PUBLIC_USER_DESCRIPTION,
-            designation: NEXT_PUBLIC_USER_DESIGNATION,
-            location: NEXT_PUBLIC_USER_LOCATION,
-            github: NEXT_PUBLIC_GITHUB_URL,
-            daily: NEXT_PUBLIC_DAILY_URL,
-            email: NEXT_PUBLIC_EMAIL_TO,
+            name: process.env.NEXT_PUBLIC_USER_NAME,
+            description: process.env.NEXT_PUBLIC_USER_DESCRIPTION,
+            designation: process.env.NEXT_PUBLIC_USER_DESIGNATION,
+            location: process.env.NEXT_PUBLIC_USER_LOCATION,
+            github: process.env.NEXT_PUBLIC_GITHUB_URL,
+            daily: process.env.NEXT_PUBLIC_DAILY_URL,
+            email: process.env.NEXT_PUBLIC_EMAIL_TO,
         });
 }
