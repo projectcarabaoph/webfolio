e3904f6a661a1e5c74b5e850d2100a987b0727ad
projectcarabaoph <projectcarabao@gmail.com>
2025-07-05 22:09:19 +0800

    style: marketing header

--- app/(marketing)/_components/marketing-header.tsx
+++ app/(marketing)/_components/marketing-header.tsx
@@ -7,8 +7,8 @@ import { MainLogo } from "@/public/assets/svgs"
 
 export default function MarketingHeader() {
     return (
-        <header className="  p-2 px-4 w-full max-w-7xl border-b border-[#27272a]">
-            <nav className="w-full h-full ">
+        <header className="bg-neutral-950 z-10 p-2 px-4 w-full sticky  top-0 md:col-span-2 flex justify-center border-b border-[#27272a]">
+            <nav className="w-full h-full max-w-7xl ">
                 <ul className="list-none p-0 flex justify-between items-center">
                     <li className="flex flex-row gap-2 justify-between items-center">
                         <Link href="/">
