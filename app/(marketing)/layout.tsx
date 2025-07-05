5bab666c4b2c4e7b3640b82a935b1376aff8de67
projectcarabaoph <projectcarabao@gmail.com>
2025-07-05 16:33:09 +0800

    style & refactor: marketing layout remove footer, change button colors, border colors in nav

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -10,48 +10,29 @@ import type { ReactNode } from "react"
 
 const MarketingLayout = ({ children }: { children: ReactNode }) => {
     return (
-        <div className="bg-neutral-950 bg- grid grid-cols-1 grid-rows-[64px_1fr_64px] min-h-dvh h-auto place-items-center">
-            <header className=" border border-white p-2 w-full max-w-7xl">
-                <nav className="w-full h-full">
+        <div className="bg-neutral-950 bg- grid grid-cols-1 grid-rows-[64px_1fr] min-h-dvh h-auto place-items-center">
+            <header className="  p-2 px-4 w-full max-w-7xl border-b border-[#27272a]">
+                <nav className="w-full h-full ">
                     <ul className="list-none p-0 flex justify-between items-center">
                         <li className="flex flex-row gap-2 justify-between items-center">
                             <Link href="/">
-                                <MainLogo className="w-8 h-8 fill-neutral-400 hover:fill-neutral-200" />
+                                <MainLogo className="w-8 h-8  fill-[rgb(177,158,239)] hover:fill-neutral-200" />
                             </Link>
                         </li>
                         <li className="flex flex-row gap-2 justify-between items-center">
-                            <Button className="bg-neutral-900 text-neutral-400 hover:text-neutral-200" >
+                            <Button className="bg-[#170d27] uppercase  border border-[#392e4e]  text-[rgb(177,158,239)] hover:text-neutral-200" >
                                 Resume
                             </Button>
-                            <Button className="bg-neutral-900 text-neutral-400 hover:text-neutral-200" >
+                            <Button className="bg-[#170d27]  border border-[#392e4e]  text-[rgb(177,158,239)] hover:text-neutral-200" >
                                 <Sun />
                             </Button>
                         </li>
                     </ul>
                 </nav>
             </header>
-            <main className="w-full h-full border border-white">
+            <main className="w-full h-full ">
                 {children}
             </main>
-            <footer className="border border-white p-2 w-full  max-w-7xl">
-                <nav className="w-full h-full">
-                    <ul className="list-none p-0 flex justify-between items-center">
-                        <li className="flex flex-row gap-2 justify-between items-center">
-                            <Link href="/">
-                                <MainLogo className="w-8 h-8 fill-neutral-400 hover:fill-neutral-200" />
-                            </Link>
-                        </li>
-                        <li className="flex flex-row gap-2 justify-between items-center">
-                            <Button className="bg-neutral-900 text-neutral-400 hover:text-neutral-200" >
-                                Resume
-                            </Button>
-                            <Button className="bg-neutral-900 text-neutral-400 hover:text-neutral-200" >
-                                <Sun />
-                            </Button>
-                        </li>
-                    </ul>
-                </nav>
-            </footer>
         </div>
     )
 }
