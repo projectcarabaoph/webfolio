325ab8dad01f6c92cf7ddaa2917baa87190a2b9d
projectcarabaoph <projectcarabao@gmail.com>
2025-07-05 22:10:30 +0800

    refactor: marketing layout

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,14 +1,21 @@
 
 import type { ReactNode } from "react"
 import MarketingHeader from "@/app/(marketing)/_components/marketing-header"
+import MarketingProfileContainer from "@/app/(marketing)/_components/marketing-profile-container"
 
 const MarketingLayout = ({ children }: { children: ReactNode }) => {
     return (
-        <div className="bg-neutral-950 bg- grid grid-cols-1 grid-rows-[64px_1fr] min-h-dvh h-auto place-items-center">
-            <MarketingHeader />
-            <main className="w-full h-full ">
-                {children}
-            </main>
+        <div className="bg-neutral-950 flex justify-center">
+            <div
+                className="bg-neutral-950 w-full max-w-7xl grid grid-cols-1 grid-rows-[64px_auto_1fr] md:grid-cols-[360px_1fr] md:grid-rows-[64px_1fr]  min-h-dvh h-auto px-2 gap-2"
+            >
+                <MarketingHeader />
+                <MarketingProfileContainer />
+                <main className="w-full h-full">
+                    {children}
+                </main>
+
+            </div>
         </div>
     )
 }
